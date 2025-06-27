e2aa6f4bb4ef2ba886a7472bf376b0292e623975
TuanMinh2512 <minhvipka2k3@gmail.com>
2025-06-27 10:40:51 +0700

    feat: add time for player

--- Assets/Scripts/TimeLevel/LevelTimer.cs
+++ Assets/Scripts/TimeLevel/LevelTimer.cs
@@ -6,9 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelTimer : MonoBehaviour
 {
-    public float timeLimit = 60f;
+    [SerializeField]private float timeLimit = 60f;
+    [SerializeField]private TextMeshProUGUI timerText;
+
     private float timeLeft;
-    public TextMeshProUGUI timerText;
     private bool levelCompleted = false;
 
     private void Start()
@@ -20,12 +21,13 @@ public class LevelTimer : MonoBehaviour
     {
         if (levelCompleted) return;
 
-        timeLeft -= Time.deltaTime;
-        if (timerText) timerText.text = $"Level Time Left: {Mathf.Max(0, timeLeft):F1}s";
-
+        timeLeft -= Time.deltaTime; //cái này do m?i máy load khác nhau, nên g?i nh? này thì nó tr? ?úng v?i time th?c
+        if (timerText)
+        {
+            timerText.text = $"Time: {Mathf.Max(0, timeLeft):F1}s";
+        }
         if (timeLeft <= 0)
         {
-            PermanentUI.perm.FullReset();
             SceneManager.LoadScene("Level1");
         }
     }
